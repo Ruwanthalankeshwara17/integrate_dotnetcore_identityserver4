c3a436318e7bbe944e4315e786cf70bf395cb16d
Ruwanthalankeshwara17 <73569666+Ruwanthalankeshwara17@users.noreply.github.com>
2020-11-01 15:11:00 +0530

    Add files via upload

---
+++ identity/Configuration/Config.cs
@@ -0,0 +1,82 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace identity.Configuration
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource
+                {
+                    Name = "api",
+                    Scopes = new List<string> {"api.read", "api.write"},
+                    ApiSecrets = new List<Secret> {new Secret("ApiSecret".Sha256())},
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new[]
+            {
+                new ApiScope("api.read", "Read Access to API"),
+                new ApiScope("api.write", "Write Access to API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "demo-client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List<Secret> {new Secret("demoPassword".Sha256())}, 
+                    AllowedScopes = new List<string> { "api.read" }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser> {
+                new TestUser {
+                    SubjectId = "02D28200-CFDF-4C41-912B-812A5E1065E4",
+                    Username = "demouser",
+                    Password = "password",
+                    Claims = new List<Claim> {
+                        new Claim(JwtClaimTypes.Email, "demouser@demouser.com"),
+                        new Claim(JwtClaimTypes.Role, "admin")
+                    }
+                }
+            };
+        }
+    }
+}
